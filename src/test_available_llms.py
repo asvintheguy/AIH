ce38a79a077eb02567bb9819aa061dd194c51162
AsvinTheGuy <72887534+asvintheguy@users.noreply.github.com>
2025-04-24 20:38:14 -0500

    Update test_available_llms.py

--- src/test_available_llms.py
+++ src/test_available_llms.py
@@ -3,10 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-client = OpenAI(
-  base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-  api_key="AIzaSyDg0uPj-GOtVoCH2M2M5ON1UnR-yjZLip8",
-)
+client = OpenAI()
 
 models = client.models.list()
 for model in models:
